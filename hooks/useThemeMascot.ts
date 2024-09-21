9f4228d2546988fefb09dab6a7e15ca0a7796136
Maruquitus <marcospauloda.s.s@gmail.com>
2024-09-21 20:04:02 -0300

    feat: cleanup and build setup

--- hooks/useThemeMascot.ts
+++ hooks/useThemeMascot.ts
@@ -1,14 +1,14 @@
-import { useColorScheme } from "react-native";
+import { useColorScheme } from 'react-native';
 export function useThemeMascot(inverted?: boolean) {
   const darkMode = useColorScheme();
 
   if (inverted !== undefined) {
-    return darkMode !== "dark"
-      ? require("../assets/images/logo-preta.png")
-      : require("../assets/images/logo-branca.png");
+    return darkMode !== 'dark'
+      ? require('../assets/images/black-logo.png')
+      : require('../assets/images/white-logo.png');
   } else {
-    return darkMode === "dark"
-      ? require("../assets/images/logo-preta.png")
-      : require("../assets/images/logo-branca.png");
+    return darkMode === 'dark'
+      ? require('../assets/images/black-logo.png')
+      : require('../assets/images/white-logo.png');
   }
 }
